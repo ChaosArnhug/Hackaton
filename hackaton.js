c8bb06e91ea294aaf76b8626ad585bb28d3f5d3e
ChaosArnhug <71312671+ChaosArnhug@users.noreply.github.com>
2021-10-29 22:23:27 +0200

    Add files via upload

---
+++ hackaton.js
@@ -0,0 +1,7 @@
+let dico_mat_mail ={
+    /*matricule : email*/
+    "HE122558" : "HE122558@students.ephec.be",
+    "HE123456" : "HE123456@students.ephec.be",
+    "HE654321" : "HE654321@students.ephec.be",
+
+}
