110f66c0d58aebe3457217c9e4b3c06cc3dc2fd1
simon3421maes <92895999+simon3421maes@users.noreply.github.com>
2021-10-30 00:04:49 +0200

    Add files via upload

---
+++ new profile.js
@@ -0,0 +1,21 @@
+let listMatricule = [
+    {
+        "HE122558": "HE122558@students.ephec.be",
+        "date": "12-10-21",
+        "heure": "10h30",
+        "allerRetour": true, //true = aller, false = retour
+        "conducteur": false, //true = conducteur, false = passager 
+        "codePostal": 1345,
+    }
+
+    {
+        "HE123456": "HE123456@students.ephec.be",
+        "date": "15-9-21",
+        "heure": "9h30",
+        "allerRetour": true, //true = aller, false = retour
+        "conducteur": false, //true = conducteur, false = passager 
+        "codePostal": 1345,
+    }
+
+
+]
