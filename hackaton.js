ea627f4bbf98a1814ccead8f224107af7f5b21c5
JHespelLouis <93406828+JHespelLouis@users.noreply.github.com>
2021-10-29 23:20:51 +0200

    Update hackaton.js

--- hackaton.js
+++ hackaton.js
@@ -3,38 +3,21 @@ let dico_mat_mail ={
     "HE122558" : "HE122558@students.ephec.be",
     "HE123456" : "HE123456@students.ephec.be",
     "HE654321" : "HE654321@students.ephec.be",
-
 }
 
-function date(form){
-    if(verifierInfo()) {
-        let a = form.matricule.value;
-        let b = form.date.value;
-        let c = form.heure.value;
-        let d = form.carOrNot.value;
-        let e = form.allerRetour.value;
+
+
+
+let liste=[];
+function data(){
+    let object ={
+        matricule : form.matricule.value,
+        date : form.date.value,
+        aller : form.allerRetour.value,
+        adresse : form.address.value,
     }
-    return false
-}
-
-function verifierInfo{
-    verifMatr(form.matricule.value)
+    liste.push(object);
+    return false;
+]
     
-}
-function verifMatr(matr){
-    let ver=0
-    if(matr.length===8) {
-        if ((matr[0] === "H") && (matr[2] === "E")) {
-            for (let i = 2; i < 8; ++i) {
-                if (+matr[i].type === number) {
-                    ++ver
-                }
-            }
-        }
-    }
-    if(ver===6) {
-        return true
-    }
-}
-
 
