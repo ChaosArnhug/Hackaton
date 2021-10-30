98ccd3d7a977d55d43e82b540299328add1a3109
euanlc <82823220+euanlc@users.noreply.github.com>
2021-10-30 17:25:36 +0200

    Update hackathon.js

--- js/hackathon.js
+++ js/hackathon.js
@@ -133,15 +133,22 @@ function data(form){
 function update(dico){
     let section = document.getElementById("profiles");
     let html = "";
+    function update(dico){
+    let section = document.getElementById("profiles");
+    let html = "";
     for(let ids in Object.keys(dico)){        
-        html += "<article><table class=\"profileTable\"><thead><tr><td colspan=\"2\"><p>Annonce #"+ids+"</p></td></tr></thead>"
-        html += "<tr><th><p>Matricule :</p></th><td><p>"+dico[ids].matricule;
-        html += "</p></td></tr><tr><th><p>Date :</p></th><td><p>"+dico[ids].date;
-        html += "</p></td></tr><tr><th><p>Aller/Retour :</p></th><td><p>"+dico[ids].aller;
-        html += "</p></td></tr><tr><th><p>Adresse :</p></th><td><p>"+dico[ids].adresse;
-        html += "</p></td></tr><tr><th><p>Nombre de passager :</p></th><td><p>"+dico[ids].numPassenger;
-        html += "</p></td></tr><tr><th colspan = \"2\"><button onclick='confirmation("+ids+")'> Choisir</button></th></tr></table></article>";
+      if(dico[ids].numPassenger>0){
+            html += "<article><table class=\"profileTable\"><thead><tr><td colspan=\"2\"><p>Annonce #"+ids+"</p></td></tr></thead>"
+            html += "<tr><th><p>Matricule :</p></th><td><p>"+dico[ids].matricule;
+            html += "</p></td></tr><tr><th><p>Date :</p></th><td><p>"+dico[ids].date;
+            html += "</p></td></tr><tr><th><p>Aller/Retour :</p></th><td><p>"+dico[ids].aller;
+            html += "</p></td></tr><tr><th><p>Adresse :</p></th><td><p>"+dico[ids].adresse;
+            html += "</p></td></tr><tr><th><p>Nombre de passager :</p></th><td><p>"+dico[ids].numPassenger;
+            html += "</p></td></tr><tr><th colspan = \"2\"><button onclick='confirmation("+ids+")'> Choisir</button></th></tr></table></article>";
+        }
     }
+    section.innerHTML=html;
+}
     section.innerHTML=html;
 }
 
