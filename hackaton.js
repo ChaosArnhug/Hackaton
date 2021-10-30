4660cf1881346efc53ccda72591ae0251183e6c4
euanlc <82823220+euanlc@users.noreply.github.com>
2021-10-30 10:56:27 +0200

    Update hackaton.js

--- hackaton.js
+++ hackaton.js
@@ -19,6 +19,8 @@ function data(form){
     document.getElementById("form").reset();
     return false;
 }
+
+let extraTime = 15;
 function add(dico){
     let section = document.getElementById("profiles");
     let html = "";
@@ -28,25 +30,8 @@ function add(dico){
         html += "</p></td></tr><tr><th><p>Aller/Retour :</p></th><td><p>"+dico[i].aller;
         html += "</p></td></tr><tr><th><p>Adresse :</p></th><td><p>"+dico[i].adresse;
         html += "</p></td></tr><tr><th><p>Nombre de passager :</p></th><td><p>"+dico[i].numPassenger;
-        html += "</p></td></tr><tr><th colspan = \"2\"><button onclick=\"choose("+i+")\">Choisir</button></th></tr></table></article>";
+        html += "</p></td></tr><tr><th colspan = \"2\"><button onclick=\"sendConfirmation("+dico[i].matricule+")\">Choisir</button></th></tr></table></article>";
     }
-    section.innerHTML=html;
-}
-let profiles = true;
-
-function switcher(){
-    if(profiles){
-        document.getElementById("profiles").style.display="block";
-        document.getElementById("formInput").style.display="none";
-        console.log("changed");
-        profiles = false;
-    }else{
-        document.getElementById("formInput").style.display="flex";
-        document.getElementById("profiles").style.display="none";
-        profiles = true;
-        console.log("changed");
-    }
-}
     section.innerHTML=html;
 }
 let profiles = true;
